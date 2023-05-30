5bfdd1d313608b2a324fd63ab7ba2aeec1b80266
Pablo <pablito@me.com>
2023-05-30 19:49:42 +0100

    API call to TFL and sample data files.
    A simple API written in the frontend in App.js.
    Data is filtered and mapped to return only arrivals that are due in the
    next 60 seconds, with selected data returned and sorted in order
    of arrival time.
    Data samples have been collected and stored in sampleData folder for
    analysis and test use.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tubeData, setTubeData] = useState([]);
+
+  const lines = "bakerloo,central,circle,district,hammersmith-city,jubilee,metropolitan,northern,piccadilly,victoria,waterloo-city";
+
+  useEffect(() => {
+    axios.get(`https://api.tfl.gov.uk/Line/${lines}/Arrivals?`)
+      .then(response => {
+        const data = response.data;
+        const filteredData = data
+          .filter(item => item.timeToStation < 60)
+          .map(item => ({
+            id: item.id,
+            stationName: item.stationName,
+            lineName: item.lineName,
+            timeToStation: item.timeToStation
+          }));
+        const sortedData = filteredData.sort((a, b) => a.timeToStation - b.timeToStation);
+        setTubeData(sortedData);
+        // Save sortedData to localStorage
+        // localStorage.setItem('sortedData', JSON.stringify(sortedData));
+      })
+      .catch(error => {
+        console.error('Error fetching tube data:', error);
+      });
+  }, []);
+
+  // Display tube data
+  console.log(tubeData);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h2>London Underground</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Symphony Orchestra</h2>
       </header>
     </div>
   );

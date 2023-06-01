583d657759e0a6ae416b8c88d7e392651c0bcc47
Sam White <sam.white745@gmail.com>
2023-06-01 14:43:04 +0100

    Created map component out of tube map svg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,58 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import TubeMap from './components/TubeMap/TubeMap.js';
 
 const dataBlockDuration = 5; // seconds
 
 function App() {
-  const [tubeData, setTubeData] = useState([]);
 
-  const lines = "bakerloo,central,circle,district,hammersmith-city,jubilee,metropolitan,northern,piccadilly,victoria,waterloo-city";
-
-  useEffect(() => {
-    const fetchData = () => {
-      axios.get(`https://api.tfl.gov.uk/Line/${lines}/Arrivals?`)
-        .then(response => {
-          const data = response.data;
-          const filteredData = data
-            .filter(item => item.timeToStation < dataBlockDuration)
-            .map(item => ({
-              id: item.id,
-              stationName: item.stationName,
-              lineName: item.lineName,
-              timeToStation: item.timeToStation
-            }));
-          const sortedData = filteredData.sort((a, b) => a.timeToStation - b.timeToStation);
-          setTubeData(sortedData);
-          // Save sortedData to localStorage
-          // localStorage.setItem('sortedData', JSON.stringify(sortedData));
-        })
-        .catch(error => {
-          console.error('Error fetching tube data:', error);
-        });
-    };
-
-    // Fetch data on first render
-    fetchData();
-
-    // Fetch data every <dataBlockDuration> seconds
-    const interval = setInterval(fetchData, (dataBlockDuration * 1000));
-
-    // Clean up the interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {  // Display tube data whenever it changes
-    console.log(tubeData);
-  }, [tubeData]);
+  var fade_state = true;
+  const fadeElement = (elementId) => {
+    const element = document.getElementById(elementId);
+    console.log(element.id);
+    if (fade_state == true) {
+      console.log("Fade out");
+      element.style.animation = "fade-out 1s forwards";
+      fade_state = false;
+    } else if (fade_state == false) {
+      console.log("Fade In");
+      element.style.animation = "fade-in 1s forwards";
+      fade_state = true;
+    }
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>London Underground</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Nil-harmonic Orchestra</h2>
-      </header>
+    <div >
+      <button type="button" onClick={() => fadeElement("Central")}>Central Fade</button>
+      <TubeMap/>
     </div>
   );
 }
---
+++ frontend/src/components/TubeMap/TubeMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ReactComponent as Map} from '../../assets/FreshTube.svg';
+
+const TubeMap = () => {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+export default TubeMap;

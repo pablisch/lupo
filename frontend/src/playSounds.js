565c33abd475e338160dff5f5638a0258c6830e1
Pablo <pablito@me.com>
2023-06-05 16:39:28 +0100

    moved App.js into App folder

--- frontend/src/playSounds.js
+++ frontend/src/playSounds.js
@@ -9,19 +9,19 @@ const playSounds = (quantisedTubeData, instruments) => {
       const note = assignNoteForVictoriaLine(train.stationName);
       setTimeout(() => {
         instruments.Victoria.triggerAttackRelease(note, '8n');
-        // console.log(`${train.stationName} - ${train.lineName} line. Time To Station: ${train.timeToStation}`);
+        console.log(`${train.stationName} - ${train.lineName} line. Time To Station: ${train.timeToStation}`);
       }, train.timeToStation * 1000);
     } else if (train.lineName === 'Jubilee') {
       const note = assignNoteForJubileeLine(train.stationName);
       setTimeout(() => {
         instruments.Jubilee.triggerAttackRelease(note, '4n');
-        // console.log(`${train.stationName} - ${train.lineName} line. Time To Station: ${train.timeToStation}`);
+        console.log(`${train.stationName} - ${train.lineName} line. Time To Station: ${train.timeToStation}`);
       }, train.timeToStation * 1000);
     } else if (train.lineName === 'Northern') {
       const note = assignNoteForNorthernLine(train.stationName);
       setTimeout(() => {
         instruments.Northern.triggerAttackRelease(note, '8n');
-        // console.log(`${train.stationName} - ${train.lineName} line. Time To Station: ${train.timeToStation}`);
+        console.log(`${train.stationName} - ${train.lineName} line. Time To Station: ${train.timeToStation}`);
       }, train.timeToStation * 1000);
     }
   })

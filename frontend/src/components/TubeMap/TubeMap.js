7a90112276e04b93718c99ea19c151fea8351950
Pablo <pablito@me.com>
2023-07-22 09:37:09 +0100

    Some experimentation with map starting position

--- frontend/src/components/TubeMap/TubeMap.js
+++ frontend/src/components/TubeMap/TubeMap.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import {ReactComponent as Map} from '../../assets/Tube.svg';
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+// import React from 'react';
+// import {ReactComponent as Map} from '../../assets/Tube.svg';
+// import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 // import "./TubeMap.css";
+
+// const TubeMap = () => {
+//   return (
+//     <main>
+//       <div className="map-wrapper">
+//       <TransformWrapper
+//         disablePadding={false}
+//         // centerOnInit={true}
+//         initialScale={1.7}
+//         initialPositionX={-400}
+//         initialPositionY={-300}
+//       >
+//         <TransformComponent wrapperStyle={{
+//             width: "100%",
+//             height: "100%",
+//           }}
+//           contentStyle={{ width: "100%", height: "100%" }}>
+//           <Map />
+//         </TransformComponent>
+//       </TransformWrapper>
+//       </div>
+//     </main>
+//   );
+// }
+// export default TubeMap;
+
+import React from 'react';
+import { ReactComponent as Map } from '../../assets/Tube.svg';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import './TubeMap.css';
 
 const TubeMap = () => {
   return (
     <main>
-      <TransformWrapper
-        disablePadding={false}
-        // centerOnInit={false}
-        initialScale={1.7}
-        initialPositionX={-400}
-        initialPositionY={-300}>
-        <TransformComponent wrapperStyle={{
+      <div className="map-wrapper">
+        <TransformWrapper
+          disablePadding={false}
+          initialScale={1.7}
+          initialPositionX={0}
+          initialPositionY={0}
+        >
+          <TransformComponent wrapperStyle={{
             width: "100%",
             height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
           }}
-          contentStyle={{ width: "100%", height: "100%" }}>
-          <Map />
-        </TransformComponent>
-      </TransformWrapper>
+          contentStyle={{
+            width: "100%",
+            height: "100%",
+            transformOrigin: "center",
+            position: "absolute"
+          }}>
+            <Map />
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
     </main>
   );
 }
+
 export default TubeMap;
